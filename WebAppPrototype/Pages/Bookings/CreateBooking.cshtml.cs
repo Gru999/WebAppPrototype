7305ee21669f43beba2302c9a55fd309d0de1c41
fbaun <fbaun@HIGHTOWER>
2022-11-28 22:28:10 +0100

    Add project files.

---
+++ WebAppPrototype/Pages/Bookings/CreateBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppPrototype.Pages.Bookings
+{
+    public class CreateBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebAppPrototype/Pages/Bookings/DeleteBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppPrototype.Pages.Bookings
+{
+    public class DeleteBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
